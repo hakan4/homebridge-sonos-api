9b202876aff0a1405fc129c93165f128121a8fb6
Håkan Andersson <hakan4@kth.se>
2017-01-03 21:13:40 +0100

    Inital commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var request = require("request");
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    homebridge.registerAccessory("homebridge-sonos", "Sonos-api", SonosAccessory);
+}
+
+function SonosAccessory(log, config) {
+    this.log = log;
+    this.config = config;
+    this.name = config["name"];
+
+    this.service = new Service.Lightbulb(this.name);
+    this.service
+        .getCharacteristic(Characteristic.On)
+        .on('get', this.getOn.bind(this))
+        .on('set', this.setOn.bind(this));
+}
+
+SonosAccessory.prototype.getOn = function(callback) {
+    request.get({
+        url: 'http://localhost:5005/Living Room/status'
+    }, function(err, response, body) {
+        callback(null, body.playbackState === 'PLAYING'; );
+    }.bind(this));
+}
+
+SonosAccessory.prototype.setOn = function(on, callback) {
+    const url = on ? 'favorite/starred' : 'stop';
+    request.get({
+        url: 'http://localhost:5005/Living Room/' + url
+    }, function(err, response, body) {
+        callback(null, on);
+    }.bind(this));
+}
+
+SonosAccessory.prototype.getServices = function() {
+    return [this.service];
+}
