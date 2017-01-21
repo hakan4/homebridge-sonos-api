c645bfecb3bce70032b3657e8faf07c27691f3c2
Håkan Andersson <hakan4@kth.se>
2017-01-21 13:03:12 +0100

    Added a new dynamic way to build up accessories for all favorite playlists and rooms

--- index.js
+++ index.js
@@ -8,35 +8,62 @@ module.exports = function(homebridge) {
 }
 
 function SonosAccessory(log, config) {
+    console.log("CONFIG: ", config);
+
     this.log = log;
     this.config = config;
     this.name = config["name"];
 
-    this.service = new Service.Lightbulb(this.name);
-    this.service
-        .getCharacteristic(Characteristic.On)
-        .on('get', this.getOn.bind(this))
-        .on('set', this.setOn.bind(this));
+    const rooms = config.rooms;
+    const playlists = config.playlists;
+    const services = rooms.map(room => {
+        const playlistServices = playlists.map( playlist => {
+            const service = new Service.Lightbulb(room + ' ' + playlist, room.toLowerCase() + playlist.toLowerCase());
+            service.getCharacteristic(Characteristic.On)
+                .on('get', setupGetOn(room, playlist))
+                .on('set', setupSetOn(room, playlist));
+            return service;
+        });
+        return playlistServices;
+    });
+
+    this.services = services.reduce((a, b) => { return a.concat(b); }) 
 }
 
-SonosAccessory.prototype.getOn = function(callback) {
-    request.get({
-        url: 'http://localhost:5005/Living Room/state'
-    }, function(err, response, body) {
-        const state = JSON.parse(body);
-        callback(null, state.playbackState === 'PLAYING');
-    }.bind(this));
+
+function setupGetOn(roomName, playlist) {
+    console.log('created getOn  callback for room: ' + roomName);
+    return (callback) => {
+        console.log('function getOn call for room: ' + roomName);
+        request.get({
+            url: 'http://localhost:5005/' + roomName + '/state'
+        }, (error, response, body) => {
+            if(!error) {
+                const state = JSON.parse(body);
+                callback(null, state.playbackState === 'PLAYING');
+            } else {
+                console.log('Error: ' + error);
+                callback(null, false);
+            }
+        });
+    }
 }
 
-SonosAccessory.prototype.setOn = function(on, callback) {
-    const url = on ? 'favorite/starred' : 'pause';
-    request.get({
-        url: 'http://localhost:5005/Living Room/' + url
-    }, function(err, response, body) {
-        callback(null, on);
-    }.bind(this));
+function setupSetOn(roomName, playlist) {
+    console.log('created setOn callback for room: ' + roomName);
+    return (on, callback) => {
+        console.log('function setOn call for room: ' + roomName);
+        request.get({
+            url: 'http://localhost:5005/' + roomName + '/' + (on ? 'favorite/' + playlist : 'pause')
+        }, (error, response, body) => {
+            if(error) { 
+                console.log('Error: ' + err);
+            }
+            callback(null, on);
+        });
+    }
 }
 
 SonosAccessory.prototype.getServices = function() {
-    return [this.service];
+    return this.services;
 }
